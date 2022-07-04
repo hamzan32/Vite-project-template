b5b1ef7e7e8100154460f159dd01d5a7c632dcd6
hamzan32 <36395457+hamzan32@users.noreply.github.com>
2022-07-04 22:39:40 +0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import './index.scss';
 import 'antd/dist/antd.less';
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { ClickToComponent } from 'click-to-react-component';
+// import { ClickToComponent } from 'click-to-react-component';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -10,11 +10,13 @@ import { store } from '@/redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.Fragment>
-    <BrowserRouter>
-      <Provider store={store}>
+    <Suspense fallback={<h1>Loading ....</h1>}>
+      <BrowserRouter>
         <App />
-        <ClickToComponent />
-      </Provider>
-    </BrowserRouter>
+        {/* <Provider store={store}>
+
+      </Provider> */}
+      </BrowserRouter>
+    </Suspense>
   </React.Fragment>
 );
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import chatSocketService from '@/services/chat-socket.service';
 import { api } from '@/redux/socketMutation';
+import { Link, Navigate } from 'react-router-dom';
 function LandingPage() {
   useEffect(() => {
     chatSocketService.listen(`user-joined`).subscribe((response) => {
@@ -12,7 +13,11 @@ function LandingPage() {
   }, []);
   // const count = useSelector((state: RootState) => state.counter.value);
   // const dispatch = useDispatch();
-  return <h1>sarwar</h1>;
+  return (
+    <>
+      <Link to="auth/sign-in">signin</Link>
+    </>
+  );
 }
 
 export default LandingPage;
