85aedf4cb463548cb27649943416380e48a688fb
hamzan32 <36395457+hamzan32@users.noreply.github.com>
2022-07-04 22:05:43 +0500

    dakh le bhi

--- src/components/Auth/SignIn/SignIn.tsx
+++ src/components/Auth/SignIn/SignIn.tsx
@@ -83,7 +83,19 @@ function SignIn() {
           </Form>
         </Col>
         <Col className="right-side" span={12}>
-          col-12
+          <div className="content">
+            <h2>HSI_TALK</h2>
+            <h3>IMAGINE A PLACE...</h3>
+            <p>
+              where you can belong to a school club, a gaming group, or a
+              worldwide art community. Where just you and a handful of friends
+              can spend time together. A place that makes it easy to talk every
+              day and hang out more often.
+            </p>
+            <Button className="signup" type="text">
+              Sign Up
+            </Button>
+          </div>
         </Col>
       </Row>
     </>
