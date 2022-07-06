fabc39e0368602a32cf667bf00c81f28d38caf18
hamzan32 <36395457+hamzan32@users.noreply.github.com>
2022-07-06 13:23:23 +0500

    fchange

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -11,25 +11,25 @@ function Auth() {
           <Outlet />
         </Col>
         <Col className="right-side" span={12}>
-          {/* <div className="content"> */}
-          <h2>HSI_TALK</h2>
-          <h3>IMAGINE A PLACE...</h3>
-          <p>
-            where you can belong to a school club, a gaming group, or a
-            worldwide art community. Where just you and a handful of friends can
-            spend time together. A place that makes it easy to talk every day
-            and hang out more often.
-          </p>
-          {location.pathname.indexOf('sign-up') == -1 ? (
-            <Button className="signup" type="text">
-              <Link to="sign-up">Sign-Up</Link>
-            </Button>
-          ) : (
-            <Button className="signup" type="text">
-              <Link to="sign-in">Sign-In</Link>
-            </Button>
-          )}
-          {/* </div> */}
+          <div className="content">
+            <h2>HSI_TALK</h2>
+            <h3>IMAGINE A PLACE...</h3>
+            <p>
+              where you can belong to a school club, a gaming group, or a
+              worldwide art community. Where just you and a handful of friends
+              can spend time together. A place that makes it easy to talk every
+              day and hang out more often.
+            </p>
+            {location.pathname.indexOf('sign-up') == -1 ? (
+              <Button className="signup" type="text">
+                <Link to="sign-up">Sign-Up</Link>
+              </Button>
+            ) : (
+              <Button className="signup" type="text">
+                <Link to="sign-in">Sign-In</Link>
+              </Button>
+            )}
+          </div>
         </Col>
       </Row>
     </>
