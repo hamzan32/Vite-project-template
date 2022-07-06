c80fc7413ca9a4f81e6a93f172a876e353814152
Muhammad Sarwar <rrsarwar109@gmail.com>
2022-07-06 21:31:16 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
-app.get('*', express.static('./dist', { maxAge: '1y' }));
+app.get('*.*', express.static('./dist', { maxAge: '1y' }));
+app.all('*', function (req, res) {
+  res.status(200).sendFile(`/`, { root: './dist' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
